1f5e69e970b043a7ed4574d70af08d5a4a110b49
MarcoGarcia20 <josu1901456@gmail.com>
2026-06-30 19:12:06 -0500

    Cambio en entrypoint.sh

--- server/docker-laravel/entrypoint.sh
+++ server/docker-laravel/entrypoint.sh
@@ -6,6 +6,10 @@ echo "Configurando Apache para escuchar en el puerto: $PORT_NUMBER"
 sed -i "s/Listen 80/Listen $PORT_NUMBER/g" /etc/apache2/ports.conf
 sed -i "s/<VirtualHost \*:80>/<VirtualHost \*:$PORT_NUMBER>/g" /etc/apache2/sites-available/000-default.conf
 
+# Forzar limpieza de cachés antiguas
+php artisan config:clear
+php artisan cache:clear
+
 echo "Optimizando caché de configuración y rutas de Laravel..."
 php artisan config:cache
 php artisan route:cache
